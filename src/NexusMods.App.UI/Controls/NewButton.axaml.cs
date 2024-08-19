35f197366cddc321ff24fb0cf3981b9eb072208c
insomnious <simon.davies@nexusmods.com>
2024-08-19 09:49:57 +0100

    button updates

--- src/NexusMods.App.UI/Controls/NewButton.axaml.cs
+++ src/NexusMods.App.UI/Controls/NewButton.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
 using Avalonia.Controls.Primitives;
 using Avalonia.Layout;
 using NexusMods.Icons;
@@ -23,6 +24,8 @@ public class NewButton : Button
     private UnifiedIcon? _leftIcon  = null;
     private UnifiedIcon? _rightIcon = null;
     private TextBlock? _label = null;
+    private ContentPresenter? _content = null;
+    private Border? _border = null;
     
     public static readonly StyledProperty<string?> TextProperty = AvaloniaProperty.Register<NewButton, string?>(nameof(Text), defaultValue: "New Button");
     public static readonly StyledProperty<IconValue?> LeftIconProperty = AvaloniaProperty.Register<NewButton, IconValue?>(nameof(LeftIcon), defaultValue: IconValues.ChevronDown);
@@ -97,13 +100,27 @@ public class NewButton : Button
         _leftIcon = e.NameScope.Find<UnifiedIcon>("PART_LeftIcon");
         _rightIcon = e.NameScope.Find<UnifiedIcon>("PART_RightIcon");
         _label = e.NameScope.Find<TextBlock>("PART_Label");
+        _content = e.NameScope.Find<ContentPresenter>("PART_ContentPresenter");
+        _border = e.NameScope.Find<Border>("PART_Border");
 
-        if (_leftIcon == null || _rightIcon == null || _label == null) return;
+        if (_leftIcon == null || _rightIcon == null || _label == null || _content == null || _border == null) return;
 
         _leftIcon.Value = LeftIcon;
         _rightIcon.Value = RightIcon;
 
         _label.IsVisible = ShowLabel;
+
+        // so we can use the traditional button as well as our own properties to set the button
+        if (Content is null)
+        {
+            _content.IsVisible = false;
+            _border.IsVisible = true;
+        }
+        else
+        {
+            _content.IsVisible = true;
+            _border.IsVisible = false;
+        }
 
         switch (VisibleIcon)
         {
